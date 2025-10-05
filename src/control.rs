80ffba296555a65f02060f889aeca0294b8925eb
Georges Palauqui <georges@foundationdevices.com>
2025-10-05 12:42:30 +0200

    fix bug

--- src/control.rs
+++ src/control.rs
@@ -15,9 +15,9 @@ bitflags! {
     #[derive(Debug, Copy, Clone)]
     pub struct PollFlags: u8 {
         /// Data can be read from the ST25R95 when set.
-        const CAN_READ = 1 << 2;
+        const CAN_READ = 1 << 3;
 
         /// Data can be sent to the ST25R95 when set.
-        const CAN_SEND = 1 << 3;
+        const CAN_SEND = 1 << 2;
     }
 }
